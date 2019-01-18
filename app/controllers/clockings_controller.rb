2e9c4d9e46a972bf0b2a9796af8e4d19dbe83a60
soni17 <elb004@gmail.com>
2019-01-18 15:02:37 -0500

    add create actions in clockings controller

--- app/controllers/clockings_controller.rb
+++ app/controllers/clockings_controller.rb
@@ -8,4 +8,21 @@ class ClockingsController < ApplicationController
     @clocking = Clocking.new
   end
 
+  def create
+    @clocking = Clocking.new(clocking_params)
+    @clocking.time = Time.now
+    if @clocking.save
+      flash[:success] = "Clocked #{params[:clocking][:clock_type]} succesfully"
+      redirect_to clockings_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def clocking_params
+    params.require(:clocking).permit(:name , :clock_type)
+  end
+
 end
